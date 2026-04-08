ef4642d6fc8abec26fb1c905343b1f0109a0ca61
Niteesh gandham <niteesh38@gmail.com>
2026-04-08 22:13:30 +0530

    content added01

---
+++ helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is just hello world program in shell, just for information purpose
+echo "Hello world"
+echo "Welcome to shell scripting"
+echo "This is a simple shell script"
+echo "You can run this script using the command: ./helloworld.sh"
+echo "Make sure to give execute permission to the script using the command: chmod +x helloworld.sh"
+echo "Enjoy learning shell scripting!" 
