ccac52fc198dbb23ba733f95c6c7e5e453b872da
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 16:58:15 +0530

    action

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script demonstrates the use of variables in shell scripting
+
+person1=$1
+person2=$2
+
+ #printing the person1 and person2 variable by providing arguments while running the script
+echo "name of person1 is $person1"
+echo "name of person2 is $person2"
