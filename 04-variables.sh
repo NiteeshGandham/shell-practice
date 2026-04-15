3a80783efff5588a43033febd962362e4fb3102b
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 17:13:18 +0530

    action

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script demonstrates the use of variables in shell scripting
+# Variable declaration using commands in variable assignment
+
+startdate=$(date +%s)
+echo "Script started at: $startdate"
+
+enddate=$(date +%s)
+echo "Script ended at: $enddate"
+duration=$((enddate - startdate))
+echo "Script execution time: $duration seconds"
