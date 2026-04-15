eec420f48b3e27d86a8cf61c50d3d9dde4beca5d
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 17:09:25 +0530

    action

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script demonstrates the use of variables in shell scripting
+# Variable declaration
+echo "Please enter Username"
+read username
+
+echo "Please enter password"
+read -s password
+
+echo "Username is: $username"
+echo "Password is: $password"
+
