48d1c2feea9483f313cd7995adba1b6c53f982fa
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 17:03:32 +0530

    action

--- 02-variables.sh
+++ 02-variables.sh
@@ -8,3 +8,8 @@ person2=$2
  #printing the person1 and person2 variable by giving #1 and #2 as arguments
 echo "name of person1 is $person1"
 echo "name of person2 is $person2"
+
+echo "$person1 : Hello $person2, how are you?"
+echo "$person2 : Hi $person1, I am good. How about you?"
+echo "$person1 : I am doing well, thank you for asking!"
+echo "$person2 : That's great to hear! Let's catch up soon."    
