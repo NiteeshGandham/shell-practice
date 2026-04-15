6ecbcfb379a693404eecc1c1d66410a12962e5a5
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 17:43:27 +0530

    action

---
+++ 06-datatypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#data types demonstration in shell scripting
+
+num1=10
+num2=niteesh
+
+echo "The value of num1 is: $num1"
+echo "The value of num2 is: $num2"
+sum=$(($num1 + $num2))
+echo "The sum of num1 and num2 is: $sum"
