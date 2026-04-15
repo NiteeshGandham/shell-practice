39ed38900ad92a8e940f65d435866dd509e10d85
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 17:51:37 +0530

    action

---
+++ 07_conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#conditions demonstration in shell scripting
+#!/bin/bash
+
+echo "Please enter a number"
+read num
+
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal
+# -ne -> not equal
+
+if [ $num -gt 20 ]; then
+	echo "$num is greater than 20"
+elif [ $num -eq 20 ]; then
+	echo "$num is equal to 20"
+else	
+	echo "$num is less than 20
+fi 
