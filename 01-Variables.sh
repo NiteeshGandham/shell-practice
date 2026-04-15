4cc446a4faca07b56bbf2943d8f2a47439fb3206
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 16:45:37 +0530

    action

---
+++ 01-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#varibale declarition
+
+Person1="John"
+person2="Doe"
+#printing the person1 and person2 variable
+Echo "name of person1 is $person1"
+Echo "name of person2 is $person2"
