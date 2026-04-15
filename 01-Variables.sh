aab019bc11ea30e76f3e08c72950e5e9787962d5
Niteesh gandham <niteesh38@gmail.com>
2026-04-15 16:48:55 +0530

    action

--- 01-Variables.sh
+++ 01-Variables.sh
@@ -5,5 +5,5 @@
 Person1="John"
 person2="Doe"
 #printing the person1 and person2 variable
-Echo "name of person1 is ::" $person1
-Echo "name of person2 is::" $person2
+echo "name of person1 is ::" $person1
+echo "name of person2 is::" $person2
