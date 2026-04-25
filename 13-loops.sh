d9cee013d127fa4970423a08ce811556d3ee445c
Niteesh gandham <niteesh38@gmail.com>
2026-04-25 17:20:18 +0530

    action

---
+++ 13-loops.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+LOG_FOLDER=/var/log/shellscript
+LOG_FILE=/var/log/shellscript/$0.log
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ];then
+	echo "Please log in with root user"
+	exit 1
+fi
+
+mkdir -p $LOGFILE
+
+validate(){
+	if [ $1 -ne 0 ];then
+		echo "$2 installation.....FAILURE"
+		exit 1
+	else 
+		echo "$2 installation.....SUCCESS"
+	fi
+}
+
+
+for package in $@
+do 
+	dnf install $package -y | tee -a $LOGFILE
+	validate $? "$package"
+done
